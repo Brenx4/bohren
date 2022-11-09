e30c20c391e507175adf32055fb5c0bdb3ef1399
darwhais <darwhais@kkg-do.eu>
2022-11-09 15:04:24 +0100

    Initial commit

---
+++ src/bohren.java
@@ -0,0 +1,15 @@
+public class bohren {
+    private int watt;
+    private String name;
+    private int age;
+    private double price;
+    private boolean broken;
+
+    public bohren(){
+        this.name= name;
+        this.watt= 659598;
+        this.age= 17;
+        this.price= 50;
+        this.broken=false;
+    }
+}
